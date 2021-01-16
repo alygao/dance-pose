92e033f5fad65c08ba86a516426a35d7505cecd8
colinywu <colin.y.wu@gmail.com>
2021-01-16 01:59:50 -0500

    Handling Input Files

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './home.css';
+//posenet
 
 function Home() {
-  function handleSubmit() {
-    console.log("hi")
+  let professionalInput = React.createRef();
+  let ownInput = React.createRef();
+
+  let [professionalVideo, setProfessionalVideo] = useState(null);
+  let [ownVideo, setOwnVideo] = useState(null);
+
+  function handleSubmit(event) {
+    setProfessionalVideo(professionalInput.value);
+    setOwnVideo(ownInput.value);
+    console.log("running");
+    event.preventDefault();
   }
   
   return (
@@ -18,19 +28,31 @@ function Home() {
             <div className="choose-file-button">
               <label className="custom-file-upload">
                 <input type="file"
-                  id="image"
-                  accept="image/png, image/jpeg"                  size="60"
-                  required
-                  />
-              choose a file
+                  id="file"
+                  size="60"
+                  required 
+                  ref={professionalInput}/>
+              choose professional video file
               </label>
             </div>
+
+            <div className="choose-file-button">
+              <label className="custom-file-upload">
+                <input type="file"
+                  id="file"
+                  size="60"
+                  required 
+                  ref={ownInput}/>
+              choose your own video file
+              </label>
+            </div>
+            
           </p>
 
 
           <div className="submit-button">
             <label className="submit-file">
-              <input type="submit" size="60" />
+              <input type="submit" size="60" value="Submit" />
                 submit
               </label>
           </div>
